7d91145b1aa0ddf9ac5145437d292bb6009ad908
clj198606061111@163.com <clj-1986>
2024-04-06 22:31:17 +0800

    kafka 数据源

--- src/main/java/com/itclj/source/KafkaSourceDemo.java
+++ src/main/java/com/itclj/source/KafkaSourceDemo.java
@@ -6,6 +6,10 @@ import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+/**
+ * kafka部署和使用命令
+ * https://note.youdao.com/web/#/file/E50C4FCCEC434C66B6EACB224A57383D/markdown/3AFE19EFCD74471BB00A295408766517/
+ */
 public class KafkaSourceDemo {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -13,9 +17,9 @@ public class KafkaSourceDemo {
 
         //从kafka读数据
         KafkaSource kafkaSource = KafkaSource.<String>builder()
-                .setBootstrapServers("")
+                .setBootstrapServers("192.168.0.105:9092")
                 .setGroupId("itclj-flink-kafka-group")
-                .setTopics("topic_01")
+                .setTopics("itcljtest")
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .setStartingOffsets(OffsetsInitializer.latest())
                 .build();
