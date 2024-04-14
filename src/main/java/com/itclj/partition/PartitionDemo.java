2e3f542865c0af76f0bfa5543167e9fec067d6c7
ice chen <itclj@itclj.com>
2024-04-14 10:35:32 +0800

    flink 分区

--- src/main/java/com/itclj/partition/PartitionDemo.java
+++ src/main/java/com/itclj/partition/PartitionDemo.java
@@ -15,7 +15,17 @@ public class PartitionDemo {
         //sockDS.shuffle().print();
 
         //rebalance 轮训：
-        sockDS.rebalance().print();
+        //如果是数据源倾斜的场景，source读进来后，调用rebalance,就可以解决数据源的数据倾斜
+        //sockDS.rebalance().print();
+
+        //rescale缩放：实现轮训，局部组队，比rebalance更高效
+        //sockDS.rescale().print();
+
+        //broadcast 广播，发送给下游所有子任务
+        sockDS.broadcast().print();
+
+        //global 全局：全部发往第一个任务。
+        sockDS.global().print();
 
 
         env.execute();
