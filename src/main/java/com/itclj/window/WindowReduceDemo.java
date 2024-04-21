e79a35cfc52c7f8b0615e01322f3c56b160f9ea3
ice chen <itclj@itclj.com>
2024-04-21 08:34:40 +0800

    flink window function reduce

--- src/main/java/com/itclj/window/WindowReduceDemo.java
+++ src/main/java/com/itclj/window/WindowReduceDemo.java
@@ -2,6 +2,7 @@ package com.itclj.window;
 
 import com.itclj.bean.WaterSensor;
 import com.itclj.functions.WaterSensorMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.datastream.WindowedStream;
@@ -21,8 +22,15 @@ public class WindowReduceDemo {
         KeyedStream<WaterSensor, String> sensorKS = sensorDS.keyBy(sensor -> sensor.getId());
 
         WindowedStream<WaterSensor, String, TimeWindow> sensorWS = sensorKS.window(TumblingProcessingTimeWindows.of(Time.seconds(5)));
-        sensorWS.reduce()
+        SingleOutputStreamOperator<WaterSensor> reduce = sensorWS.reduce(new ReduceFunction<WaterSensor>() {
+            @Override
+            public WaterSensor reduce(WaterSensor v1, WaterSensor v2) throws Exception {
+                System.out.println("调用Reduce方法，value1=" + v1.getVc() + ",value2=" + v2.getVc());
+                return new WaterSensor(v1.getId(), v2.getTs(), v1.getVc() + v2.getVc());
+            }
+        });
 
+        reduce.print();
 
         env.execute();
     }
