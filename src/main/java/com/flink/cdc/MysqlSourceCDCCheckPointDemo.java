f7f6544f6ecc0e4a1dba0d2372fc726de76bb0dd
ice chen <itclj@itclj.com>
2024-05-04 07:26:03 +0800

    flink-cdc mysql source check point

---
+++ src/main/java/com/flink/cdc/MysqlSourceCDCCheckPointDemo.java
@@ -0,0 +1,51 @@
+package com.flink.cdc;
+
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackendFactory;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class MysqlSourceCDCCheckPointDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        //1. 启用checkpointing ,默认是barrier对象，,间隔为5000毫米,检查模式为Exactly once
+        env.enableCheckpointing(5000, CheckpointingMode.EXACTLY_ONCE);
+        env.setStateBackend(new FsStateBackend("file:///D:/bigdata/flink/data/state-backend"));
+        CheckpointConfig checkpointConfig = env.getCheckpointConfig();
+        //2. 设置检查点存储位置，
+        checkpointConfig.setCheckpointStorage("file:///D:/bigdata/flink/data/checkpoint");
+        //3. checkpoint的超时时间：默认10分钟
+        checkpointConfig.setCheckpointTimeout(60000);//设置60秒
+        //4. 同时运行中的checkpoint的最大数量
+        checkpointConfig.setMaxConcurrentCheckpoints(1);
+        //5. 最小等待间隔：上一轮checkpoint结束 到下一轮checkpoint开始之间的间隔，设置了 > 0,并发会变成1
+        checkpointConfig.setMinPauseBetweenCheckpoints(1000);
+        //6. 取消作业时，checkpoint的数据 是否 保留在外边系统
+        checkpointConfig.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //7. 允许 checkpoint 连续失败的次数，默认0，表示，表示checkpointy一次失败，job就挂掉
+        checkpointConfig.setTolerableCheckpointFailureNumber(0);
+
+        MySqlSource<String> source = MySqlSource.<String>builder()
+                .hostname("127.0.0.1")
+                .port(3306)
+                .username("root")
+                .password("123456")
+                .databaseList("itclj")
+                .tableList("itclj.tags")
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .startupOptions(StartupOptions.initial())
+                .build();
+
+        DataStreamSource<String> outDataStreamSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "mysql-cdc");
+        outDataStreamSource.print();
+
+        env.execute();
+    }
+}
